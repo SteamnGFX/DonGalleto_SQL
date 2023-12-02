fe730af701f67633361a2c4d509f6ffe8dc7d4c5
Ángel Martínez <51030210+SteamnGFX@users.noreply.github.com>
2023-12-02 09:34:45 -0600

    Update 1 Tablas.sql
    Se agregaron tabla de Venta

--- 1 Tablas.sql
+++ 1 Tablas.sql
@@ -19,11 +19,24 @@ id INT AUTO_INCREMENT PRIMARY KEY,
 cantidad INT,
 nombre VARCHAR(255),
 descripcion VARCHAR(255),
-fotografia longtext
+fotografia longtext,
+precio float
 );
 
-INSERT INTO inventario_galleta (cantidad, nombre, descripcion,fotografia) VALUES
-(10, 'Galleta de chocolate', 'Galleta de chocolate con textura suave, relleno delicioso',""),
-(5, 'Galleta de fresa', 'Galleta de vainilla con textura suave, relleno delicioso',""),
-(15, 'Galleta de hallowen', 'Galleta conmemorativa de hallowen',"");
+create table Venta(
+idVenta INT PRIMARY KEY AUTO_INCREMENT,
+idGalleta INT NOT NULL,
+precioGalleta INT NOT NULL,
+nombreGalleta VARCHAR(255) NOT NULL,
+cantidad INT NOT NULL,
+tipo INT NOT NULL,
+fecha VARCHAR(255) NOT NULL,
+
+FOREIGN KEY (idGalleta) REFERENCES inventario_galleta(id)
+);
+
+INSERT INTO inventario_galleta (cantidad, nombre, descripcion,fotografia,precio) VALUES
+(10, 'Galleta de chocolate', 'Galleta de chocolate con textura suave, relleno delicioso',"", "100"),
+(5, 'Galleta de fresa', 'Galleta de vainilla con textura suave, relleno delicioso',"","100"),
+(15, 'Galleta de hallowen', 'Galleta conmemorativa de hallowen',"","100");
     
